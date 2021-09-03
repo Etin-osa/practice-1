b8f6c0910ba1f711230866aa66502ef6f63c160b
Etin <frederickade15@gmail.com>
2021-09-03 17:15:23 +0200

    Js changes
    Cleaning our js code making it easier to read

--- logic.js
+++ logic.js
@@ -1,17 +1,26 @@
-const circle = $('.circle');
+//  DOM Variable(s)
+  // Header Variable(s) 
 const listIcon = $('.header-list__icon');
-const navLeft = $('.grid-nav__left');
 const menuText = $('.menu-text');
-const viewLink = $('.grid-view__inn a');
 const search = $('.header-list__search-inner input')
-const menuIcon = $('.header-menu__icon');
-const navRight = $('.grid-nav__right');
-const mainNote = $('.grid-main__note');
 const hamburger = $('#hamburger');
+const menuIcon = $('.header-menu__icon');
 const menuBlocks = $('.menu-block');
 const headerMenu = $('.header-menu');
 const cancel = $('.header-menu__cancel');
 const sups = $('.menu-block__text sup');
+
+  // Grid Variable(s)
+const navLeft = $('.grid-nav__left');
+const viewLink = $('.grid-view__inn a');
+const navRight = $('.grid-nav__right');
+const mainNote = $('.grid-main__note');
+
+  // Circle
+const circle = $('.circle');
+
+
+// Not DOM Variable(s)
 const slots = [
   {
     img: $('.main-img__1'),
@@ -37,8 +46,7 @@ const slots = [
 ];
 const mouseResponse = [navLeft, navRight, cancel, listIcon, menuText, viewLink, menuIcon, search];
 
-
-// Numbers and strings
+  // Number(s) and string(s)
 let count = 0;
 let navActive = '#ECECEC';
 let navFaint = 'rgba(236, 236, 236, .6)';
@@ -50,209 +58,65 @@ let add = 'add';
 let remove = 'remove';
 
 
-// Mouse movement
-$(document).mousemove(function(e) {
-  let x = e.pageX;
-  let y = e.pageY;
-
-  circle.css({
-    'top': `${y - 35}px`,
-    'left': `${x - 35}px`,
-    'opacity': '1',
-    'transform': 'scale(1)'
-  });
-});
-
-// Mouse movement
-$(document).mouseleave(function(e) {
-  circle.css({
-    'opacity': '0',
-    'transform': 'scale(0)'
-  })
-})
 
 
-// Mouse effect
-mouseResponse.forEach((res) => mouseEffect(res))
 
 
-// NAVRIGHT
-navRight.click(function() {
+// Function(s)
+  // Start function
+function start() {
+  const allList = $('.main-head__child');
+  const allMenu = $('.menu-text');
 
-  if (count < 2) {
-    // Before count
-    // Change image
-    changeImage(add, count);
 
-    // Add animation Main Span
-    addAnimation(count, add, slotMain);
+  // Note(s)
+  const text1 = ['INTERIOR', 'EFFICIENT', 'AFFORDABLE', 'DESIGN', 'DESIGN', 'PRICES'];
+  const text2 = ['INTERIOR DESIGN', 'EFFICIENT DESIGN', 'AFFORDABLE PRICES'];
 
-    // line Animation
-    lineAnimation(count, add, slotLine, 'right');
+  // Dom change(s)
+  parentLoop(allList, text1, slotMain);
+  parentLoop(allMenu, text2, slotMenu);
+}
 
-    // Number Animation
-    lineAnimation(count, add, slotNumber, 'right');
-    
-    // Increment
-    count++
-    
-    // After count
-    // Note animation
-    noteAnimation(mainNote);
-    
-    // Change image
-    changeImage(remove, count);
-    
-    // Add animation Main Span
-    addAnimation(count, remove, slotMain);
-    
-    // line Animation
-    lineAnimation(count, remove, slotLine, 'right');
+  // Parent loop
+function parentLoop(arr, str, slot) {
+  for (var i = 0; i < arr.length; i++) {
+    // Create && Add span to parent
+    createSpans(arr[i], str[i]);
 
-    // Number Animation
-    lineAnimation(count, remove, slotNumber, 'right');
-
-    // Change icon colour
-    iconColourChange(count);
-
-    // LineToFill
-    lineToFill(count);
+    // Add parent to slots
+    addToSlot(arr[i], slot, i);
   }
+}
 
-  // Lazy fix to one text Animation
-  setTimeout(() => {
-    mainNote.css('animation', 'none');
-  }, 2000)
-});
+  // Create span(s) && Append 
+function createSpans(dom, str) {
+  for (var i = 0; i < str.length; i++) {
+    const span = document.createElement('span');
+    span.textContent = str[i];
 
+    // Checking for empty span
+    if (str[i] === ' ') {
+      span.style.padding = '0 .35vw'
+    }
 
-// NAVLEFT
-navLeft.click(function() {
-
-  if (count > 0) {
-    // Before count
-    // Change image
-    changeImage(add, count);
-
-    // addAnimation
-    addAnimation(count, add, slotMain);
-
-    // line Animation
-    lineAnimation(count, add, slotLine, 'left');
-
-    // Number Animation
-    lineAnimation(count, add, slotNumber, 'left');
-    
-    // Decrement
-    count--
-    
-    // Aafter count
-    // NoteAnimation
-    noteAnimation(mainNote);
-    
-    // Change image
-    changeImage(remove, count)
-    
-    // addAnimation
-    addAnimation(count, remove, slotMain);
-
-    // line Animation
-    lineAnimation(count, remove, slotLine, 'left');
-
-    // Number Animation
-    lineAnimation(count, remove, slotNumber, 'left');
-    
-    // Change icon colour
-    iconColourChange(count);
-
-    // LineToFill
-    lineToFill(count);
+    // Append span
+    dom.append(span);
   }
+}
 
-  // Lazy fix to one text Animation
-  setTimeout(() => {
-    mainNote.css('animation', 'none');
-  }, 2000)
-})
+  // Add Span(s) to Respective Slot(s)
+function addToSlot(dom, slot, numb) {
+  const domId = dom.getAttribute('id');
+  const selectSpans = $(`#${domId} span`);
+  let curSlot = numb <= 2 ? numb : numb - 3;
 
-
-// Hamburger
-hamburger.click(() => {
-  // Header Menu
-  headerMenu.addClass('open')
-
-  // Menu List
-  slots.forEach((slot, ind) => addAnimation(ind, remove, slotMenu));
-
-  // Menu list hightlight
-  sups.css('opacity', '1')
-});
-
-// Header cancel
-cancel.click(() => {
-  // headerMenu
-  headerMenu.removeClass('open');
-
-  // MenuList
-  slots.forEach((slot, ind) => addAnimation(ind, add, slotMenu));
-
-  // Menu list hightlight
-  sups.css('opacity', '0')
-});
+  slots[curSlot][slot].push(selectSpans);
+}
 
 
-// Menu Text
-Array.from(menuText).forEach((text, ind) => text.addEventListener('click', () => {
-  let dir = count > ind ? 'left' : 'right';
-
-  // HeaderMenu remove class
-  headerMenu.removeClass('open');
-
-  // Change Image
-  changeImage(add, count);
-
-  // addAnimation
-  addAnimation(count, add, slotMain);
-
-  // line Animation
-  lineAnimation(count, add, slotLine, dir);
-
-  // NumberAnimation
-  lineAnimation(count, add, slotNumber, dir);
-
-  // Increment
-  count = ind;
-
-  // NoteAnimation
-  noteAnimation(mainNote);
-
-  // change image
-  changeImage(remove, count);
-
-  // Add main span animation
-  addAnimation(count, remove, slotMain);
-
-  // line Animation
-  lineAnimation(count, remove, slotLine, dir);
-
-  // line Animation
-  lineAnimation(count, remove, slotNumber, dir);
-
-  // line Animation
-  iconColourChange(count);
-
-  // LineToFill
-  lineToFill(count);
-  
-  // Lazy fix to one text Animation
-  setTimeout(() => {
-    mainNote.css('animation', 'none');
-  }, 2000)
-}))
-
-
-
-// Change Image Function
+//Animation Function(s) 
+  // Change Image Function
 function changeImage(str, ind) {
   if (str === add) {
     slots[ind].img.css({
@@ -269,8 +133,7 @@ function changeImage(str, ind) {
   }
 }
 
-
-// Change icon colour
+  // Change Navigation Icon Colour
 function iconColourChange(numb) {
   if (numb > 0) {
     navLeft.css('background', navActive);
@@ -289,9 +152,8 @@ function iconColourChange(numb) {
   }
 }
 
-
-// Add Aniamtion Function
-function addAnimation(cur, time, props) {
+  // Animation Loop Function
+function animationLoop(cur, time, props) {
   const spanList = slots[cur][props];
   const duration = 0.7
   let eachSec = props === slotMain ? 0.03 : 0.01;
@@ -313,8 +175,7 @@ function addAnimation(cur, time, props) {
   })
 }
 
-
-// Animate span
+  // Animate span
 function animateSpan(cur, pace, time, props) {
   let val = time === add ? 100 : 10;
   let del = time === add ? 0 : props === slotMenu ? .7 : 1.3;
@@ -323,28 +184,28 @@ function animateSpan(cur, pace, time, props) {
   cur.style.transition = `transform .3s ease ${del + pace}s`;
 }
 
-// Note Animation
+  // Note Animation
 function noteAnimation(dom) {
   dom.css({
     'animation': 'textAnimation 1.2s ease-out forwards .4s'
   })
 }
 
+  // Line To Fill
 function lineToFill(curIndex) {
   const textArr = Array.from(menuBlocks);
 
   textArr.forEach((text, ind) => {
-    ind === curIndex ? 
+    ind === curIndex ?
       text.classList.add('open') :
       text.classList.remove('open');
   })
 }
 
-
-// line & number animation
+  // Line && Number Animation
 function lineAnimation(cur, time, props, dir) {
   let del = time === add ? .7 : 1.4;
-  let val = time === add ? 
+  let val = time === add ?
     dir === 'right' ? -100 : 100 : 0;
   let dom = slots[cur][props];
 
@@ -355,7 +216,7 @@ function lineAnimation(cur, time, props, dir) {
   })
 }
 
-// MouseEffect
+  // Circle change(s) on Links
 function mouseEffect(dom) {
   dom.mousemove(() => circle.css({
     'background': '#ececec27',
@@ -369,58 +230,173 @@ function mouseEffect(dom) {
 }
 
 
-// function to Create spans and ADD to parent 
-function createSpans(dom, str) {
-  for ( var i = 0; i < str.length; i++ ) {
-    const span = document.createElement('span');
-    span.textContent = str[i];
-    
-    // Checking for empty span
-    if (str[i] === ' ') {
-      span.style.padding = '0 .35vw'
-    }
-
-    // Append span
-    dom.append(span);
-  }
-}
-
-
-// Function to Add parent to slots
-function addToSlot(dom, slot, numb) {
-  const domId = dom.getAttribute('id');
-  const selectSpans = $(`#${domId} span`);
-  let curSlot = numb <= 2 ? numb : numb - 3;
-
-  slots[curSlot][slot].push(selectSpans);
-}
-
-
-// Funciton to create multiple spans
-function mulCreate(arr, str, slot) {
-  for ( var i = 0; i < arr.length; i++ ) {
-    // Create && Add span to parent
-    createSpans(arr[i], str[i]);
-
-    // Add parent to slots
-    addToSlot(arr[i], slot, i);
-  }
-}
-
-// Start function
-function start() {
-  const allList = $('.main-head__child');
-  const allMenu = $('.menu-text');
-
-
-  // Note(s)
-  const text1 = ['INTERIOR', 'EFFICIENT', 'AFFORDABLE', 'DESIGN', 'DESIGN', 'PRICES'];
-  const text2 = ['INTERIOR DESIGN', 'EFFICIENT DESIGN', 'AFFORDABLE PRICES'];
-
+// Dom Change Function
   // Dom change(s)
-  mulCreate(allList, text1, slotMain);
-  mulCreate(allMenu, text2, slotMenu);
+function domChanges(curIndex, curDom, curDir) {
+  // Change Image
+  changeImage(curDom, curIndex);
+
+  // Animation Loop
+  animationLoop(curIndex, curDom, slotMain);
+
+  // Line Animation
+  lineAnimation(curIndex, curDom, slotLine, curDir);
+
+  // Number Animation
+  lineAnimation(curIndex, curDom, slotNumber, curDir);
 }
+
+
+
+
+// Event Listener
+  // Circle Mousemove Listener
+$(document).mousemove(function(e) {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  circle.css({
+    'top': `${y - 35}px`,
+    'left': `${x - 35}px`,
+    'opacity': '1',
+    'transform': 'scale(1)'
+  });
+});
+
+  // Circle Mouseleave listener
+$(document).mouseleave(function(e) {
+  circle.css({
+    'opacity': '0',
+    'transform': 'scale(0)'
+  })
+})
+
+  // Links Mousemove && Mouseleave Listener
+mouseResponse.forEach((res) => mouseEffect(res))
+
+  // Navright click Listener
+navRight.click(function() {
+
+  if (count < 2) {
+    // Before count
+    domChanges(count, add, 'right')
+    
+    // Increment
+    count++
+    
+    // After count
+    // Note animation
+    noteAnimation(mainNote);
+    
+    // Dom change(s)
+    domChanges(count, remove, 'right');
+
+    // Change icon colour
+    iconColourChange(count);
+
+    // LineToFill
+    lineToFill(count);
+  }
+
+  // Lazy fix to one text Animation
+  setTimeout(() => {
+    mainNote.css('animation', 'none');
+  }, 2000)
+});
+
+
+  // Navleft Click Listener 
+navLeft.click(function() {
+
+  if (count > 0) {
+    // Before count
+    domChanges(count, add, 'left');
+    
+    // Decrement
+    count--
+    
+    // After count
+    // NoteAnimation
+    noteAnimation(mainNote);
+    
+    // Dom change(s) 
+    domChanges(count, remove, 'left');
+    
+    // Change icon colour
+    iconColourChange(count);
+
+    // LineToFill
+    lineToFill(count);
+  }
+
+  // Lazy fix to one text Animation
+  setTimeout(() => {
+    mainNote.css('animation', 'none');
+  }, 2000)
+})
+
+  // Hamburger Click Listener
+hamburger.click(() => {
+  // Header Menu
+  headerMenu.addClass('open')
+
+  // Menu List
+  slots.forEach((slot, ind) => animationLoop(ind, remove, slotMenu));
+
+  // Menu list hightlight
+  sups.css('opacity', '1')
+});
+
+  // Header cancel
+cancel.click(() => {
+  // headerMenu
+  headerMenu.removeClass('open');
+
+  // MenuList
+  slots.forEach((slot, ind) => animationLoop(ind, add, slotMenu));
+
+  // Menu list hightlight
+  sups.css('opacity', '0')
+});
+
+
+  // Menu Text Click Listener
+Array.from(menuText).forEach((text, ind) => text.addEventListener('click', () => {
+  let dir = count > ind ? 'left' : 'right';
+
+  // HeaderMenu remove class
+  headerMenu.removeClass('open');
+
+  // Dom Change(s)
+  domChanges(count, add, dir);
+
+  // Animation Loop Menu
+  slots.forEach((slot, ind) => animationLoop(ind, remove, slotMenu));
+
+  // Increment
+  count = ind;
+
+  // NoteAnimation
+  noteAnimation(mainNote);
+
+  // Dom changes
+  domChanges(count, remove, dir);
+
+  // Animation Loop Menu
+  slots.forEach((slot, ind) => animationLoop(ind, add, slotMenu));
+
+  // line Animation
+  iconColourChange(count);
+
+  // LineToFill
+  lineToFill(count);
+  
+  // Lazy fix to one text Animation
+  setTimeout(() => {
+    mainNote.css('animation', 'none');
+  }, 2000)
+}))
+
 
 
 // Call Start function
